ca8d8c4d46fb6ab5179578040dd8f14d9135b8c0
Tymon <tymon.wilczek@gmail.com>
2023-03-16 16:38:00 +0100

    Calculator ACTUALLY calculates now

--- formularze/v0.5/home/assets/calcs/kalkulator prosty/script.js
+++ formularze/v0.5/home/assets/calcs/kalkulator prosty/script.js
@@ -2,8 +2,9 @@ window.addEventListener("load", function () {
     var equation = "0";
     var output = 0;
     var m = 0;
+    var lastPressed = "";
     // const eqationRegex = /(?:\d+(?:\.\d+)?)(?:(?:\/|\*|\+|\-)(?:\d+(?:\.\d+)?))*/; // I don't think this is needed now but keeping it just cause
-    const eqationSplitRegex = /(\/|\*|\+|\-)/; // Regexes are easy to build hard to read. Change my mind
+    const eqationSplitRegex = /(\/|\*|\+|\−)/; // Regexes are easy to build hard to read. Change my mind
 
     class displayClass {
         updateEquation(passedEquation) {
@@ -16,7 +17,7 @@ window.addEventListener("load", function () {
                         case "*": return num1 * num2;
                         case "/": return num1 / num2;
                         case "+": return num1 + num2;
-                        case "-": return num1 - num2;
+                        case "−": return num1 - num2;
                     };
                 };
 
@@ -28,23 +29,22 @@ window.addEventListener("load", function () {
                     return indexes;
                 };
 
-                let operationsList = [];
                 let operationsOrder = [];
                 let splittedEquation = equation.split(eqationSplitRegex);
-                if (!splittedEquation[splittedEquation.length - 1]) {splittedEquation = splittedEquation.slice(0, -2)}
+                if (!splittedEquation[splittedEquation.length - 1]) { splittedEquation = splittedEquation.slice(0, -2) }
 
                 for (let i = 0; i < splittedEquation.length; i++) {
                     if (i % 2 === 0) {
                         splittedEquation[i] = parseFloat(splittedEquation[i]);
-                    } else {
-                        operationsList.push(splittedEquation[i]);
                     }
                 }
-                
+
                 while (splittedEquation.length !== 1) {
-                    operationsOrder = (getAllIndexes(operationsList, "*").concat(getAllIndexes(operationsList, "/"))).sort().concat((getAllIndexes(operationsList, "+").concat(getAllIndexes(operationsList, "-"))).sort());
-                    splittedEquation[2 * operationsOrder[0]] = doMath(splittedEquation[operationsOrder[0]], splittedEquation.splice(operationsOrder[0] + 1, 1)[0], splittedEquation.splice(operationsOrder[0] + 1, 1)[0]);
-                }
+                    operationsOrder = (getAllIndexes(splittedEquation, "*").concat(getAllIndexes(splittedEquation, "/"))).sort().concat((getAllIndexes(splittedEquation, "+").concat(getAllIndexes(splittedEquation, "−"))).sort());
+
+                    splittedEquation[operationsOrder[0] - 1] = doMath(splittedEquation[operationsOrder[0] - 1], splittedEquation.splice(operationsOrder[0], 1)[0], splittedEquation.splice(operationsOrder[0], 1)[0]);
+                    operationsOrder.shift();
+                };
                 return splittedEquation[0];
             };
 
@@ -53,12 +53,13 @@ window.addEventListener("load", function () {
         };
 
         updateOutput(passedOutput) {
-            document.getElementById("output").innerText = passedOutput;
+            document.getElementById("output").innerText = (isNaN(passedOutput) || passedOutput === Infinity ? "Błąd" : passedOutput);
         };
-        
+
         updateM(passedM) {
             const mDiv = document.getElementById("m");
             mDiv.innerText = passedM;
+            lastPressed = "";
         };
     };
     const display = new displayClass;
@@ -67,15 +68,17 @@ window.addEventListener("load", function () {
     function addNumber(passedEquation, number) {
         equation = (passedEquation === "0" ? number.toString() : equation + number.toString());
         display.updateEquation(equation);
+        lastPressed = "";
     };
 
     function addOperation(passedEquation, operation) {
         let splittedEquation = passedEquation.split(eqationSplitRegex);
-        
-        if (splittedEquation[splittedEquation.length -1]) {
+
+        if (splittedEquation[splittedEquation.length - 1]) {
             equation += operation;
         }
         display.updateEquation(equation);
+        lastPressed = "";
     }
 
     function addDot(passedEquation) {
@@ -85,41 +88,65 @@ window.addEventListener("load", function () {
             equation = passedEquation + ".";
         }
         display.updateEquation(equation);
+        lastPressed = "";
+    }
+
+    function pushHistory(passedEquation, passedOutput) {
+        const history1 = document.getElementById("history-1");
+        const history2 = document.getElementById("history-2");
+        const history3 = document.getElementById("history-3");
+
+        history3.innerText = history2.innerText;
+        history2.innerText = history1.innerText;
+        history1.innerText = `${passedEquation.split(eqationSplitRegex).join(" ")} = ${passedOutput}`
     }
 
 
     document.getElementById("mrc").addEventListener("click", () => {
-        if (m == 0) {} else if (output == m) {
+        if (m == 0) { } else if (output === m) {
             m = 0;
             display.updateM(m);
-        } else { // here insert a regex to only add m if equation doesnt end with a number
-            equation = m.toString();
+        } else {
+            let splittedEquation = equation.split(eqationSplitRegex);
+            if (!splittedEquation[splittedEquation.length - 1]) { splittedEquation = splittedEquation.slice(0, -1) }
+            if (splittedEquation.length % 2 === 0) { equation += m.toString(); }
             display.updateEquation(equation);
         }
     });
 
-    document.getElementById("m-plus").addEventListener("click", () => {display.updateM(m += output);});
-    document.getElementById("m-minus").addEventListener("click", () => {display.updateM(m -= output);});
+    document.getElementById("m-plus").addEventListener("click", () => { display.updateM(m += output); });
+    document.getElementById("m-minus").addEventListener("click", () => { display.updateM(m -= output); });
 
-    document.getElementById("ce").addEventListener("click", () => {display.updateEquation(equation = "0");});
+    document.getElementById("ce").addEventListener("click", () => { display.updateEquation(equation = "0"); lastPressed = ""; });
 
-    document.getElementById("b0").addEventListener("click", () => {addNumber(equation, 0)});
-    document.getElementById("b1").addEventListener("click", () => {addNumber(equation, 1)});
-    document.getElementById("b2").addEventListener("click", () => {addNumber(equation, 2)});
-    document.getElementById("b3").addEventListener("click", () => {addNumber(equation, 3)});
-    document.getElementById("b4").addEventListener("click", () => {addNumber(equation, 4)});
-    document.getElementById("b5").addEventListener("click", () => {addNumber(equation, 5)});
-    document.getElementById("b6").addEventListener("click", () => {addNumber(equation, 6)});
-    document.getElementById("b7").addEventListener("click", () => {addNumber(equation, 7)});
-    document.getElementById("b8").addEventListener("click", () => {addNumber(equation, 8)});
-    document.getElementById("b9").addEventListener("click", () => {addNumber(equation, 9)});
+    document.getElementById("b0").addEventListener("click", () => { addNumber(equation, 0) });
+    document.getElementById("b1").addEventListener("click", () => { addNumber(equation, 1) });
+    document.getElementById("b2").addEventListener("click", () => { addNumber(equation, 2) });
+    document.getElementById("b3").addEventListener("click", () => { addNumber(equation, 3) });
+    document.getElementById("b4").addEventListener("click", () => { addNumber(equation, 4) });
+    document.getElementById("b5").addEventListener("click", () => { addNumber(equation, 5) });
+    document.getElementById("b6").addEventListener("click", () => { addNumber(equation, 6) });
+    document.getElementById("b7").addEventListener("click", () => { addNumber(equation, 7) });
+    document.getElementById("b8").addEventListener("click", () => { addNumber(equation, 8) });
+    document.getElementById("b9").addEventListener("click", () => { addNumber(equation, 9) });
 
-    document.getElementById("div").addEventListener("click", () => {addOperation(equation, "/")});
-    document.getElementById("times").addEventListener("click", () => {addOperation(equation, "*")});
-    document.getElementById("minus").addEventListener("click", () => {addOperation(equation, "-")});
-    document.getElementById("plus").addEventListener("click", () => {addOperation(equation, "+")});
+    document.getElementById("div").addEventListener("click", () => { addOperation(equation, "/") });
+    document.getElementById("times").addEventListener("click", () => { addOperation(equation, "*") });
+    document.getElementById("minus").addEventListener("click", () => { addOperation(equation, "−") });
+    document.getElementById("plus").addEventListener("click", () => { addOperation(equation, "+") });
 
-    document.getElementById("equals").addEventListener("click", () => {display.updateEquation(equation)});
+    document.getElementById("equals").addEventListener("click", () => {
+        let splittedEquation = equation.split(eqationSplitRegex);
+        if (!splittedEquation[splittedEquation.length - 1]) { splittedEquation = splittedEquation.slice(0, -2) }
 
-    document.getElementById("dot").addEventListener("click", () => {addDot(equation)});
+        if (lastPressed === "equals" && splittedEquation.length > 2) { splittedEquation = splittedEquation.concat(splittedEquation.slice(splittedEquation.length - 2)) }
+
+        equation = splittedEquation.join('');
+        display.updateEquation(equation);
+
+        pushHistory(equation, output);
+        lastPressed = "equals";
+    });
+
+    document.getElementById("dot").addEventListener("click", () => { addDot(equation) });
 });
