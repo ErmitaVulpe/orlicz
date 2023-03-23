5bb58216a01c0d4e80a7a044243996229d95a36b
Tymon <tymon.wilczek@gmail.com>
2023-03-23 16:35:16 +0100

    doin' your mom

--- formularze/v0.5/profile/assets/script.js
+++ formularze/v0.5/profile/assets/script.js
@@ -1,8 +1,14 @@
 window.addEventListener("load", function () {
-    const db = JSON.parse(localStorage.getItem("db-users"));
     const email = localStorage.getItem("loggedInAs");
+    const db = JSON.parse(localStorage.getItem("db-users"))[email];
 
-    document.getElementById("title-welcome-text").innerText += ` ${db[email]["username"]}!`;
+    document.getElementById("title-welcome-text").innerText += ` ${db["username"]}!`;
     document.getElementById("logout").addEventListener("click", logout);
-    document.getElementById("title-container").addEventListener("click", () => {window.location.replace("../home/index.html")});
+    document.getElementById("title-container").addEventListener("click", () => { window.location.replace("../home/index.html") });
+
+    document.getElementById("username").innerText = db["username"];
+    document.getElementById("name").innerText = db["data"]["name"];
+    document.getElementById("lastname").innerText = db["data"]["lastname"];
+    document.getElementById("pesel").innerText = db["data"]["pesel"];
+    document.getElementById("nip").innerText = db["data"]["nip"];
 });
