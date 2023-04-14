7fda3420a6438fb6cad4d2e7415bf599764b6006
Tymon <tymon.wilczek@gmail.com>
2023-04-14 21:27:21 +0200

    Update 14.04.23 added editing profile details.

--- formularze/v0.5/profile/assets/script.js
+++ formularze/v0.5/profile/assets/script.js
@@ -2,13 +2,45 @@ window.addEventListener("load", function () {
     const email = localStorage.getItem("loggedInAs");
     const db = JSON.parse(localStorage.getItem("db-users"))[email];
 
-    document.getElementById("title-welcome-text").innerText += ` ${db["username"]}!`;
-    document.getElementById("logout").addEventListener("click", logout);
-    document.getElementById("title-container").addEventListener("click", () => { window.location.replace("../home/index.html") });
+    const submitMessage = document.getElementById("buttons");
+    let isSubmitMessageActive = false;
 
-    document.getElementById("username").innerText = db["username"];
-    document.getElementById("name").innerText = db["data"]["name"];
-    document.getElementById("lastname").innerText = db["data"]["lastname"];
-    document.getElementById("pesel").innerText = db["data"]["pesel"];
-    document.getElementById("nip").innerText = db["data"]["nip"];
+    document.getElementById("title-welcome-text").innerText += ` ${db["username"]} !`;
+    document.getElementById("logout").addEventListener("click", logout);
+
+    document.getElementById("title-container").addEventListener("click", () => {
+        window.location.replace("../home/index.html")
+    });
+
+    function discardChanges() {
+        document.getElementById("username").value = db["username"];
+        document.getElementById("name").value = db["data"]["name"];
+        document.getElementById("lastname").value = db["data"]["lastname"];
+        document.getElementById("pesel").value = db["data"]["pesel"];
+        document.getElementById("nip").value = db["data"]["nip"];
+    }
+    discardChanges();
+
+    document.getElementById("cancel").addEventListener("click", () => { discardChanges() });
+    document.getElementById("submit").addEventListener("click", () => {
+        console.log(JSON.parse(localStorage.getItem("db-users")));
+        db["username"] = document.getElementById("username").value;
+        db["data"]["name"] = document.getElementById("name").value;
+        db["data"]["lastname"] = document.getElementById("lastname").value;
+        db["data"]["pesel"] = document.getElementById("pesel").value;
+        db["data"]["nip"] = document.getElementById("nip").value;
+
+        let lolo = JSON.parse(localStorage.getItem("db-users"));
+        lolo[email] = db
+        localStorage.setItem("db-users", JSON.stringify(lolo));
+
+        if (!isSubmitMessageActive) {
+            submitMessage.classList.add("active");
+            isSubmitMessageActive = true;
+            setTimeout(() => {
+                submitMessage.classList.remove("active");
+                isSubmitMessageActive = false;
+            }, 2000);
+        }
+    });
 });
