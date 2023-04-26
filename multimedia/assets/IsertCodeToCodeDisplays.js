a3f74e252715ecb32c442fe6f7041e54bcdd67e3
ErmitaVulpe <tymon.wilczekWgmail.com>
2023-04-26 13:53:11 +0200

    It's starting to look good

---
+++ multimedia/assets/IsertCodeToCodeDisplays.js
@@ -0,0 +1,10 @@
+function IsertCodeToCodeDisplays() {
+    const codeDisplaysDB = [
+        `<video width="400px">\n\t<source src="link do filmiku" type="video/mp4">\n</video>`
+    ];
+
+    for (let i = 0; i < codeDisplaysDB.length; i++) {
+        document.querySelector(`#embedded-code-${i}`).innerText = codeDisplaysDB[i];
+    }
+};
+IsertCodeToCodeDisplays();
