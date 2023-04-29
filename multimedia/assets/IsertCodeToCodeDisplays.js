9a8d65970403c701299c595385ea0fa37cc2c2cf
Tymon <tymon.wilczek@gmail.com>
2023-04-29 19:58:51 +0200

    started work on images section

--- multimedia/assets/IsertCodeToCodeDisplays.js
+++ multimedia/assets/IsertCodeToCodeDisplays.js
@@ -11,7 +11,8 @@ function IsertCodeToCodeDisplays() {
         `<audio controls>\n\t<source src="{ścieżka do pliku}" type="audio/{format pliku}">\n</audio>`,
         `<iframe style="border-radius:12px" src="{link do Spotify}" width="350px" height="80"\n\tframeBorder="0" allowfullscreen="" allow="autoplay; clipboard-write;\n\tencrypted-media; fullscreen; picture-in-picture" loading="lazy">\n</iframe>`,
         `<iframe style="border-radius:12px" src="{link do Spotify}"  width="250" height="352"\n\tframeBorder="0" allowfullscreen="" allow="autoplay; clipboard-write;\n\tencrypted-media; fullscreen; picture-in-picture" loading="lazy">\n</iframe>`,
-        `<iframe width="75%" height="125" scrolling="no" frameborder="no" allow="autoplay"\n\tsrc="{link do soundcloud"></iframe>\n<div style="font-size: 10px; color: #cccccc; line-break: anywhere;word-break: normal;\n\toverflow: hidden;white-space: nowrap;\n\ttext-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans\n\tUnicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">\n</div>`
+        `<iframe width="75%" height="125" scrolling="no" frameborder="no" allow="autoplay"\n\tsrc="{link do soundcloud"></iframe>\n<div style="font-size: 10px; color: #cccccc; line-break: anywhere;word-break: normal;\n\toverflow: hidden;white-space: nowrap;\n\ttext-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans\n\tUnicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">\n</div>`,
+        `<img src="{ścieżka do pliku}">`
     ];
 
     for (let i = 0; i < codeDisplaysDB.length; i++) {
